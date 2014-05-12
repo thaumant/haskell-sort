fb6b88a0c9d420432f755a4b344f3a86d4f241e1
Eli Schelokov <thaumant@gmail.com>
2014-05-12 22:18:23 +0400

    Add merge sort

--- sort.hs
+++ sort.hs
@@ -1,4 +1,4 @@
-insertionSort :: [Int] -> [Int]
+insertionSort :: Ord a => [a] -> [a]
 insertionSort xs =
     let sort []     ys = ys
         sort (x:xs) [] = sort xs [x]
@@ -6,3 +6,15 @@ insertionSort xs =
         insert x []         = [x]
         insert x ys@(hd:tl) = if x <= hd then x:ys else hd : insert x tl
     in sort xs []
+
+
+mergeSort :: Ord a => [a] -> [a]
+mergeSort []  = []
+mergeSort [x] = [x]
+mergeSort xs  =
+    let merge [] ys = ys
+        merge xs [] = xs
+        merge (x:xs) (y:ys) = if x < y then x : merge xs (y:ys)
+                                       else y : merge (x:xs) ys
+        (left, right) = splitAt (length xs `div` 2) xs
+    in merge (mergeSort left) (mergeSort right)
