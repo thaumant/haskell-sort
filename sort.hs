317e4e4503ece600d2b0b2bc28cf0c3851c5ed65
Eli Schelokov <thaumant@gmail.com>
2014-05-13 23:26:35 +0400

    Add selection sort

--- sort.hs
+++ sort.hs
@@ -3,9 +3,21 @@ insertionSort xs =
     let sort []     ys = ys
         sort (x:xs) [] = sort xs [x]
         sort (x:xs) ys = sort xs $ insert x ys
-        insert x []         = [x]
-        insert x ys@(hd:tl) = if x <= hd then x:ys else hd : insert x tl
+        insert x []     = [x]
+        insert x (y:ys) = if x <= y then x:y:ys else y : insert x ys
     in sort xs []
+
+
+selectionSort :: Ord a => [a] -> [a]
+selectionSort [] = []
+selectionSort xs =
+    let smallestPos xs =
+            let iter p current []     = p
+                iter p current (y:ys) = let p' = if y < (xs !! p) then current else p
+                                        in  iter p' (current + 1) ys 
+            in iter 0 1 (tail xs) 
+        (left, right) = splitAt (smallestPos xs) xs
+    in head right : selectionSort (left ++ tail right)
 
 
 mergeSort :: Ord a => [a] -> [a]
